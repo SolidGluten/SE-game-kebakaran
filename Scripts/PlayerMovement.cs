b1b486c43726c6146e11c44882ba32815f821804
Colin Oliver <84835385+SolidGluten@users.noreply.github.com>
2025-02-19 13:00:40 +0700

    #feat camera smoothly following the player

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -3,8 +3,11 @@ using System;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export]
+	public float Speed = 300.0f;
+
+	[Export]
+	public float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -17,14 +20,14 @@ public partial class PlayerMovement : CharacterBody2D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump_action") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
