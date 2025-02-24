1078161553af8e92e9fac7f04f9e8051057eb646
Colin Oliver <84835385+SolidGluten@users.noreply.github.com>
2025-02-24 12:21:31 +0700

    #feat create tilesets and tilemap

---
+++ Scripts/PlayerInteraction.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class PlayerInteraction : Node
+{
+	[Export]
+	private float rayLength = 200f;
+	
+	RayCast2D raycast;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		raycast = this.GetNode<RayCast2D>("./RayCast2D");
+		raycast.TargetPosition = new Vector2(200f, 0);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(Input.IsActionJustPressed("move_left")){
+			raycast.TargetPosition = new Vector2(-200f, 0);
+		} else if(Input.IsActionJustPressed("move_right")){
+			raycast.TargetPosition = new Vector2(200f, 0);
+		}
+
+		var collider = raycast.GetCollider();
+		if(raycast.IsColliding()){
+			var colliderScript = collider.GetScript();
+			GD.Print(colliderScript is Door);			
+		}
+	}
+}
