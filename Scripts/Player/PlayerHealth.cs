cfce4b3ee727b553ad6899fbbbee48cf9b8d4dd6
Colin Oliver <84835385+SolidGluten@users.noreply.github.com>
2025-04-30 10:09:55 +0700

    #feat health UI Manager

--- Scripts/Player/PlayerHealth.cs
+++ Scripts/Player/PlayerHealth.cs
@@ -6,6 +6,8 @@ public partial class PlayerHealth : Node
 	public const int maxHealth = 3;
 	public int currentHealth;
 
+	[Signal] public delegate void OnHealthChangeEventHandler(int dmg);
+
   public override void _Ready()
   {
     currentHealth = maxHealth;
@@ -13,10 +15,11 @@ public partial class PlayerHealth : Node
 
 	public void Hurt(int dmg){
 		currentHealth = Mathf.Max(0, currentHealth - dmg);
-		GD.Print(currentHealth);
+		EmitSignal("OnHealthChange", currentHealth);		
 	}
 
 	public void Heal(int hp){
 		currentHealth = Mathf.Min(maxHealth, currentHealth + hp);
+		EmitSignal("OnHealthChange", currentHealth);		
 	}
 }
--- Scripts/UI/UIManager.cs
+++ Scripts/UI/UIManager.cs
@@ -1,18 +1,31 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class UIManager : Control
-{	
+{
 	private PlayerInventory playerInventory;
+	private PlayerHealth playerHealth;
 
 	CanvasItem fireAxeImg;
 	CanvasItem wetCloth;
 	CanvasItem fireExtinguisherImg;
 
-  public override void _Ready()
-  {
+	public string heartFullRes = "res://Sprites/heart/heart_full.png";
+	public string heartEmptyRes = "res://Sprites/heart/heart_empty.png";
+
+	public int heartCount = 3;
+	public int currentHealth = 2;
+	public List<TextureRect> heartList = new List<TextureRect>();
+
+	public override void _Ready()
+	{
 		playerInventory = GetNode<PlayerInventory>("/root/PlayerInventory");
 		playerInventory.ItemAdded += EnableItemUI;
+
+		playerHealth = GetNode<PlayerHealth>("/root/PlayerHealth");
+		playerHealth.OnHealthChange += SyncHealthUI;
 
 		fireAxeImg = GetNode<CanvasItem>("./ToolContainer/FireAxeImage");
 		wetCloth = GetNode<CanvasItem>("./ToolContainer/WetClothImage");
@@ -21,24 +34,53 @@ public partial class UIManager : Control
 		fireAxeImg.Visible = false;
 		wetCloth.Visible = false;
 		fireExtinguisherImg.Visible = false;
-  }
 
-	public void EnableItemUI(int item){
+		var children = GetNode("./HealthContainer").GetChildren().ToList();
+		foreach (var child in children)
+		{
+			if (child is TextureRect) heartList.Add((TextureRect)child);
+		}
+
+		SyncHealthUI(playerHealth.currentHealth);
+	}
+
+	public void EnableItemUI(int item)
+	{
 		ItemTypes type = (ItemTypes)Enum.ToObject(typeof(ItemTypes), item);
 
-		switch(type){
-			case ItemTypes.FireAxe:{
-				fireAxeImg.Visible = true;
-				break;
-			}
-			case ItemTypes.WetCloth:{
-				wetCloth.Visible = true;
-				break;
-			}
-			case ItemTypes.FireExtinguisher:{
-				fireExtinguisherImg.Visible = true;
-				break;
-			}
+		switch (type)
+		{
+			case ItemTypes.FireAxe:
+				{
+					fireAxeImg.Visible = true;
+					break;
+				}
+			case ItemTypes.WetCloth:
+				{
+					wetCloth.Visible = true;
+					break;
+				}
+			case ItemTypes.FireExtinguisher:
+				{
+					fireExtinguisherImg.Visible = true;
+					break;
+				}
+		}
+	}
+
+	public void SyncHealthUI(int health)
+	{
+		currentHealth = health;
+
+		foreach (var heart in heartList)
+		{
+			heart.Texture = GD.Load<Texture2D>(heartEmptyRes);
+			GD.Print("Test");
+		}
+
+		for(int i = 0; i < currentHealth; i++){
+			if (i >= heartList.Count()) break;
+			heartList[i].Texture = GD.Load<Texture2D>(heartFullRes);
 		}
 	}
 
