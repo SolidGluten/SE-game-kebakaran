cdcc685a623194cb0c903f9f205dbaed314c2987
Colin Oliver <84835385+SolidGluten@users.noreply.github.com>
2025-06-05 02:22:03 +0700

    # fix PlayerInteraction

--- Scripts/Player/PlayerInteraction.gd
+++ Scripts/Player/PlayerInteraction.gd
@@ -6,30 +6,30 @@ extends Node2D
 var player_movement
 
 func _ready():
-    player_movement = get_parent()
+	player_movement = get_parent()
 
 func _process(_delta):
-    var new_target_pos = Vector2(ray_cast_length * (player_movement.is_facing_right if player_movement.is_facing_right else -1), 0)
-    shape_cast.target_position = new_target_pos
+	var new_target_pos = Vector2(ray_cast_length * (1.0 if player_movement.is_facing_right else 0.0), 0)
+	shape_cast.target_position = new_target_pos
 
-    if Input.is_action_just_pressed("interact") and shape_cast.is_colliding():
-        var collision_count = shape_cast.get_collision_count()
-        for i in range(collision_count):
-            var collider = shape_cast.get_collider(i)
-            print(collider.name)
-            handle_collision(collider)
+	if Input.is_action_just_pressed("interact") and shape_cast.is_colliding():
+		var collision_count = shape_cast.get_collision_count()
+		for i in range(collision_count):
+			var collider = shape_cast.get_collider(i)
+			print(collider.name)
+			handle_collision(collider)
 
 func handle_collision(collider):
-    match playerInventory.get_current_item():
-        playerInventory.ItemTypes.FIRE_AXE:
-            if "Breakable" in collider.get_class():
-                collider.break()
-            print("Using Fire Axe")
-        playerInventory.ItemTypes.FIRE_EXTINGUISHER:
-            if "Extinguishable" in collider.get_class():
-                collider.extinguish()
-            print("Using Fire Extinguisher")
-        playerInventory.ItemTypes.WET_CLOTH:
-            print("Using Wet Cloth")
-        _:
-            pass
+	match playerInventory.get_current_item():
+		playerInventory.ItemTypes.FIRE_AXE:
+			if "Breakable" in collider.get_class():
+				collider.break()
+			print("Using Fire Axe")
+		playerInventory.ItemTypes.FIRE_EXTINGUISHER:
+			if "Extinguishable" in collider.get_class():
+				collider.extinguish()
+			print("Using Fire Extinguisher")
+		playerInventory.ItemTypes.WET_CLOTH:
+			print("Using Wet Cloth")
+		_:
+			pass
